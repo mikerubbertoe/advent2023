42c7908489780d30c8514dc28b50a83804b85493
Michael Roberto <mikero@vt.edu>
2023-12-08 18:59:40 -0500

    8

---
+++ day8.py
@@ -0,0 +1,58 @@
+import re
+import math
+
+def part1():
+    index = 0
+    steps = 0
+    instructions = []
+    path = dict()
+    with open("day8_input.txt", 'r') as f:
+        for line in f:
+            if not instructions:
+                instructions = [x for x in line]
+            elif line:
+                current, left, right = re.findall('([A-Z0-9]{3})', line)
+                path[current] = (left, right)
+
+        size = len(instructions)
+        current = 'AAA'
+        while current != 'ZZZ':
+            nextInstruction = instructions[index]
+            index = (index + 1) % size
+            current = path[current][0] if nextInstruction == 'L' else path[current][1]
+            steps += 1
+        print(steps)
+
+
+def part2():
+    index = 0
+    steps = 0
+    instructions = []
+    currents = []
+    leastCommon = 1
+    path = dict()
+    with open("day8_input.txt", 'r') as f:
+        for line in f:
+            if not instructions:
+                instructions = [x for x in line]
+            elif line:
+                current, left, right = re.findall('([A-Z0-9]{3})', line)
+                path[current] = (left, right)
+                if current.endswith('A'):
+                    currents.append(current)
+
+        size = len(instructions)
+        for current in currents:
+            steps = 0
+            index = 0
+            while not current.endswith('Z'):
+                nextInstruction = instructions[index]
+                index = (index + 1) % size
+                current = path[current][0] if nextInstruction == 'L' else path[current][1]
+                steps += 1
+            leastCommon = math.lcm(leastCommon, steps)
+        print(leastCommon)
+
+
+part1()
+part2()
